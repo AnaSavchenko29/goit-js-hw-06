42e37ae25e5ac6d23dde4a44647747c5c6d2024a
Анастасія Савченко <savyle4ka13@gmail.com>
2024-08-18 20:13:21 +0100

    add solutions

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+class StringBuilder {
+  #value;
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
